cd962b77d7a95e6290ef94de63870964dbfd2bad
empoweromicsapp <mobile.development@empoweromics.app>
2022-07-23 11:48:48 +0200

    Added Functionality

---
+++ index.js
@@ -0,0 +1,40 @@
+// document.querySelector(".skills .skillrow[1] h3").textContent="Player 1 Wins";
+
+
+
+
+// function getInputFromTextBox()  {
+//     // var input=document.querySelector(".form NameInput").value;
+//     // alert(input);
+//     var input=document.getElementById("Name").value;
+//     console.log(input);
+//     alert(input);
+   
+// }
+
+
+// document.querySelector(".Age").innerHTML="19";
+document.querySelector(".Email").innerHTML="ahmednaeim07@gmail.com";
+document.querySelector(".Location").innerHTML="Giza, Egypt";
+
+document.querySelector(".form")[1].innerHTML;
+
+
+
+// var randomNumber= Math.floor((Math.random() * 6) + 1);
+// document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber+".png");
+
+// var randomNumber1= Math.floor((Math.random() * 6) + 1);
+// document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber1+".png");
+
+// if(randomNumber>randomNumber1){
+//     document.querySelector(".container h1").textContent="Player 1 Wins";
+
+// }
+// else if (randomNumber==randomNumber1) {
+//     document.querySelector(".container h1").textContent="Draw";
+
+// }
+// else if(randomNumber<randomNumber1){
+//     document.querySelector(".container h1").textContent="Player 2 Wins";
+// }
